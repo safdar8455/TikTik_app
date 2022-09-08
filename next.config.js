4f6b6088bbe18ee3ebf1963a5b566772ef7ded91
Safdar Hussain <safdarse063@gmail.com>
2022-09-08 18:45:00 +0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["scontent.fkhi22-1.fna.fbcdn.net", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
